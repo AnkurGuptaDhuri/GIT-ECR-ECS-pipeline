b7b62d5ed104d158d8a6ed83089c1d3c6c218487
CloudLining <ankur5yk@gmail.com>
2024-05-13 19:53:45 +0530

    3.update the application

--- dbapipackage/__init__.py
+++ dbapipackage/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__, template_folder='templates')
+application = Flask(__name__, template_folder='templates')
+
+app = application
+
 app.config['DEBUG'] = True
 
 from dbapipackage import db
